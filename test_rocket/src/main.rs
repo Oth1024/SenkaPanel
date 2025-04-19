20eaf33b08f8b328045fea932797e089d6778632
oth1024 <2385518847@qq.com>
2025-04-19 10:06:31 +0800

    初始化测试项目

--- test_rocket/src/main.rs
+++ test_rocket/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::form::Form;
+use rocket::serde::{Serialize, Deserialize};
+use rocket::serde::json::Json;
+
+#[derive(FromForm, Serialize, Deserialize)]
+struct UserForm{
+    name: String,
+    age: u8,
 }
