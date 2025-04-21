a00fce164ed625396bff3e60a3f4cbfcc99dbba4
heyanshao <2385518847@qq.com>
2025-04-21 19:12:26 +0800

    修改配置工具

--- test_project/test_rocket/src/utilities/config_tool.rs
+++ test_project/test_rocket/src/utilities/config_tool.rs
@@ -1,20 +1,38 @@
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
 use std::{
-    collections::HashMap, fmt::format, ops::Add, path::{Path, PathBuf}, sync::{Arc, Mutex}
+    collections::HashMap,
+    fmt::format,
+    ops::Add,
+    path::{Path, PathBuf},
+    sync::{Arc, Mutex},
 };
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use tokio::fs::{self, File};
+use tokio::{fs::{self, File}, sync::watch::error};
 
 pub struct ConfigTool;
 
 impl ConfigTool {
-
-    pub async fn get_config<T>(config_name: &String, config_path: &String) -> Result<T, String> 
-    where T: DeserializeOwned
+    pub async fn get_config<T>(config_name: &String, config_path: &String) -> Result<T, String>
+    where
+        T: DeserializeOwned,
     {
-
+        let get_file = File::open(config_path).await;
+        if let Ok(open_file_success) = get_file {
+            
+        }
+        else if let Err(error_info) = get_file {
+            error!("{}", error_info);
+            return Err(String::from(error_info));
+        }
+        let error_message = "Uncoverred exception!";
+        error!("{}", error_message);
+        return Err(String::from(error_message));
     }
 
-    pub async  fn set_config<T>(config_name: &String, config_path: &String, new_config_object: &T) -> Result<(), String> {
-
+    pub async fn set_config<T>(
+        config_name: &String,
+        config_path: &String,
+        new_config_object: &T,
+    ) -> Result<(), String> {
+        return Err(String::from("Uncoverred exception!"));
     }
 }
