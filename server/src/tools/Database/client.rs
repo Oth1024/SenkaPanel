98a3c0d0ed1c4b326c16cee83712eaeb890b72c5
heyanshao <2385518847@qq.com>
2025-12-05 14:36:09 +0800

    修改文件目录

--- server/src/tools/Database/client.rs
+++ server/src/tools/Database/client.rs
@@ -1,7 +1,13 @@
 use once_cell::sync::{Lazy, OnceCell};
 use sea_orm::{Database, DatabaseConnection};
 
-use crate::{common_definitions::senka_error::{SenkaError, SenkaErrorCode}, tools::{consts::DEFAULT_DATA_DIRECTORY, logger::logger_factory::{self, SenkaLogger}}};
+use crate::{
+    common_definitions::senka_error::{SenkaError, SenkaErrorCode},
+    tools::{
+        consts::DEFAULT_DATA_DIRECTORY,
+        logger::logger_factory::{self, SenkaLogger},
+    },
+};
 
 // 连接池单例
 pub static DATABASE: OnceCell<DatabaseConnection> = OnceCell::new();
@@ -11,8 +17,17 @@ static LOGGER: Lazy<SenkaLogger> = Lazy::new(|| logger_factory::get_logger("data
 /// 初始化数据库实例
 /// 需要在程序开始时调用此方法
 pub async fn initialize_database() {
-    match Database::connect(format!("sqlite://{}db.sqlite?mode=rwc", DEFAULT_DATA_DIRECTORY)).await {
-        Ok(db) => { DATABASE.set(db).unwrap(); },
-        Err(db_err) => LOGGER.log_error(&SenkaError::new(SenkaErrorCode::Inner, db_err.to_string())),
+    match Database::connect(format!(
+        "sqlite://{}db.sqlite?mode=rwc",
+        DEFAULT_DATA_DIRECTORY
+    ))
+    .await
+    {
+        Ok(db) => {
+            DATABASE.set(db).unwrap();
+        }
+        Err(db_err) => {
+            LOGGER.log_error(&SenkaError::new(SenkaErrorCode::Inner, db_err.to_string()))
+        }
     };
 }
