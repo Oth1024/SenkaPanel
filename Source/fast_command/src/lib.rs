c61a5c9c1e7c0a8815c3e9a42cdedc4b73e7fff3
heyanshao <2385518847@qq.com>
2026-05-20 16:25:07 +0800

    添加部分实现

--- Source/fast_command/src/lib.rs
+++ Source/fast_command/src/lib.rs
@@ -1,6 +1,9 @@
-use sea_orm::{ActiveModelTrait, ActiveValue::Set, DatabaseConnection::SqlxSqlitePoolConnection};
+use common::senka_error::{SenkaError, SenkaErrorCode};
+use sea_orm::{ActiveModelTrait, ActiveValue::Set, ColumnTrait, DatabaseConnection::SqlxSqlitePoolConnection, EntityTrait, QueryFilter};
 use uuid::Uuid;
 use tools::database::client::DATABASE;
+
+use crate::fast_command::Column;
 
 pub mod fast_command;
 
@@ -21,12 +24,12 @@ pub struct FastCommandInfo {
 }
 
 impl FastCommandInfo {
-    pub fn from_database_model(model: fast_command::Model) -> Self {
+    pub fn from_database_model(model: &fast_command::Model) -> Self {
         Self {
-            uuid: model.uuid,
-            command_name: model.command_name,
-            command_value: model.command_value,
-            icon_url: model.icon_url,
+            uuid: model.uuid.clone(),
+            command_name: model.command_name.clone(),
+            command_value: model.command_value.clone(),
+            icon_url: model.icon_url.clone(),
             fast_command: model.fast_command,
             index: model.index,
         }
@@ -50,29 +53,36 @@ pub async  fn create_fast_command_info(id: u128, command_name: String, command_v
     info.insert(DATABASE.get().unwrap()).await.unwrap();
 }
 
-pub async fn get_all_fast_command_infos() -> Vec<FastCommandInfo> {
+pub async fn get_all_fast_command_infos() -> Result<Vec<FastCommandInfo>, SenkaError> {
     // 从数据库中获取所有 fast_command_info
-    Vec::<FastCommandInfo>::new()
+    let result = fast_command::Entity::find()
+        .all(DATABASE.get().unwrap()).await;
+    if let Ok(models) = result {
+        return Ok(Vec::from_iter(models.iter().map(|model|FastCommandInfo::from_database_model(model))));
+    }
+    Err(SenkaError::null())
 }
 
-pub async fn get_fast_command_detail(uuid: String) -> FastCommandInfo {
-    // 从数据库中获取 fast_command_info
-    FastCommandInfo {
-        uuid,
-        command_name: String::new(),
-        command_value: String::new(),
-        icon_url: String::new(),
-        fast_command: false,
-        index: 0,
+pub async fn get_fast_command_detail(uuid: String) -> Result<FastCommandInfo, SenkaError> {
+    let result = fast_command::Entity::find()
+        .filter(Column::Uuid.eq(uuid))
+        .one(DATABASE.get().unwrap()).await;
+    if let Ok(Some(model)) = result {
+        return Ok(FastCommandInfo::from_database_model(&model));
     }
+    Err(SenkaError::null())
 }
 
 pub async fn delete_fast_command_info(uuid: String) {
     // 从数据库中删除 fast_command_info
+    fast_command::Entity::delete_many()
+        .filter(Column::Uuid.eq(uuid))
+        .exec(DATABASE.get().unwrap()).await.unwrap();
 }
 
 pub async fn update_fast_command_info(fast_command_info: FastCommandInfo) {
     // 更新数据库中的 fast_command_info
+    
 }
 
 pub async fn execute_fast_command(uuid: String) {
