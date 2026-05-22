f63f996f02bc0bb6c63df001b7f7eb086f1dd754
Oth1024 <ouka0529@qq.com>
2026-05-22 23:04:01 +0800

    添加ProcessInfo

--- Source/process_monitor/src/lib.rs
+++ Source/process_monitor/src/lib.rs
@@ -1,8 +1,41 @@
-use tokio::process;
+use chrono::{DateTime, Utc};
+use tokio::process::Child;
+use dashmap::DashMap;
 
+#[derive(Debug)]
+pub enum ProcessStatus {
+    Running,
+    Exited(i32),
+}
+
+#[derive(Debug)]
 pub struct ProcessInfo {
-    
-    pub id: i32,
+    pub id: u32,
+    pub process: Child,
+    pub status: ProcessStatus,
+    pub command: String,
+    pub creator: String,
+    pub created_time: DateTime<Utc>,
+}
 
-    pub process: process::Command
+impl ProcessInfo {
+    pub fn new(process: Child, command: String, creator: String) -> Option<Self> {
+        if let Some(id) = process.id() {
+            return Some(ProcessInfo {
+                id: id,
+                process,
+                status: ProcessStatus::Running,
+                command,
+                creator,
+                created_time: Utc::now(),
+            });
+        }
+        else {
+            None
+        }
+    }
+}
+
+pub struct ProcessManager {
+    pub process_infos: DashMap<u32, ProcessInfo>
 }
