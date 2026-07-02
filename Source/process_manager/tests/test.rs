878af9170ea31183ed3ae2377bacefd3fab8504f
heyanshao <2385518847@qq.com>
2026-07-02 13:40:10 +0800

    添加测试

---
+++ Source/process_manager/tests/test.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    /// 测试下发单次执行的任务
+    async fn test_raw_task() {
+
+    }
+
+    #[tokio::test]
+    /// 测试进程监控的开启和关闭
+    /// 测试子进程的自动重启
+    async fn test_monitor_thread() {
+
+    }
+}
